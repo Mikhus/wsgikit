37bfdd89c57d6c82673b0293678d941085710dab
mstadnyk <mykhailo.stadnyk@globallogic.com>
2012-10-29 22:45:07 +0200

    version update

--- wsgikit/wsgikit.pyx
+++ wsgikit/wsgikit.pyx
@@ -23,7 +23,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 import sys,os,hashlib,time,random,re
 
-__version__ = "0.2a"
+__version__ = "0.2.1a"
 __author__  = ["Mykhailo Stadnyk <mikhus@gmail.com>"]
 
 class HttpRequestError(Exception):
