edc294e4267521ddfa889727456d5f1c25e5aa7f
mstadnyk <mykhailo.stadnyk@globallogic.com>
2012-10-29 16:13:14 +0200

    Setup script improved

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from wsgikit import __version__
+try :
+	from Cython.Distutils import build_ext
+except ImportError:
+	print("""You need to install Cython to build wsgikit extension.
+To install Cython, please, use:
+
+ > pip install cython
+
+or
+
+ > easy_install cython
+
+or you can download and install it manually
+from http://cython.org/#download
+""")
+	exit(1)
 
 setup(
 	name           = "wsgikit",
-	version        = "1.1b",
+	version        = __version__,
 	description    = "Python tools for WSGI applications",
 	author         = "Mykhailo Stadnyk",
 	author_email   = "mikhus@gmail.com",
@@ -15,7 +31,6 @@ setup(
 	license        = 'MIT License',
 	cmdclass       = {'build_ext': build_ext},
 	ext_modules    = [Extension("wsgikit", ["wsgikit/wsgikit.pyx"])],
-	requires       = ['cython'],
 	classifiers    = [
 		'Development Status :: 4 - Beta',
 		'Environment :: Other Environment',
@@ -23,6 +38,7 @@ setup(
 		'License :: OSI Approved :: MIT License',
 		'Natural Language :: English',
 		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
 		'Programming Language :: Python :: 3',
 		'Topic :: Software Development :: Libraries :: Python Modules',
 	],
---
+++ wsgikit/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "1.1a"
+__author__  = ["Mykhailo Stadnyk <mikhus@gmail.com>"]
